303ddae68896d77f17e6ba2489f17ad3c6ea131d
Aoalas <2874257768@qq.com>
2024-12-10 14:44:21 +0800

    TF transform.

--- include/hero_chassis_controller/hero_chassis_controller.h
+++ include/hero_chassis_controller/hero_chassis_controller.h
@@ -4,11 +4,18 @@
 #include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
 #include <geometry_msgs/Twist.h>
+#include "geometry_msgs/Vector3Stamped.h"
+#include "geometry_msgs/TransformStamped.h"
 #include <control_toolbox/pid.h>
 #include <sensor_msgs/JointState.h>
 #include <nav_msgs/Odometry.h>
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <memory>
+#include <ros/ros.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <string>
 
 namespace hero_chassis_controller {
     class HeroChassisController : public controller_interface::Controller<hardware_interface::EffortJointInterface> {
@@ -27,9 +34,9 @@ namespace hero_chassis_controller {
 
         void jointStateCallback(const sensor_msgs::JointState::ConstPtr &msg);
 
-        void updateOdometry();
-//        tf::TransformBroadcaster odom_broadcaster;
-//        tf::StampedTransform transform;
+        void updateOdometry(const sensor_msgs::JointState::ConstPtr &msg);
+        tf::TransformBroadcaster odom_broadcaster;
+        tf::TransformListener tf_listener;
 
         hardware_interface::JointHandle front_left_joint_, front_right_joint_, back_left_joint_, back_right_joint_;
 
@@ -37,9 +44,9 @@ namespace hero_chassis_controller {
 
         ros::Subscriber joint_state_sub;
         ros::Subscriber cmd_vel_sub;
+        ros::Publisher cmd_vel_pub;
         ros::Publisher odom_pub;
 
-        tf::TransformBroadcaster odom_broadcaster;
         ros::Time last_cmd_time_;
 
 
@@ -47,19 +54,25 @@ namespace hero_chassis_controller {
         double wheel_radius_;  // 车轮半径
         double wheel_base_;  //轴距
         double wheel_track_;  //轮距
+        std::string speed_mode_;  //速度模式
         double back_left_vel_ = 0;
         double front_left_vel_ = 0;
         double back_right_vel_ = 0;
         double front_right_vel_ = 0;  //速度
-        double goal_vx_ = 0.1;
-        double goal_vy_ = 0.1;
-        double goal_omega_ = 0;    //距离方向
+        double target_vx_ = 0.01;
+        double target_vy_ = 0.01;
+        double target_omega_ = 0;    //距离方向
         double x = 0.0;
         double y = 0.0;
         double th = 0.0;
-        double od_vx = 0.1;
-        double od_vy = -0.1;
-        double od_vth = 0.1;
+        double vx = 0;
+        double vy = 0;
+        double omega = 0;
+        double dx = 0;
+        double dy = 0;
+        double dth = 0;
+        geometry_msgs::Twist a_msg;
+        ros::Publisher pub;
     };
 }
 #endif //HERO_CHASSIS_CONTROLLER_HERO_CHASSIS_CONTROLLER_H
--- src/hero_chassis_controller.cpp
+++ src/hero_chassis_controller.cpp
@@ -16,8 +16,9 @@ namespace hero_chassis_controller {
 
         //获取参数配置文件里面的参数
         controller_nh.getParam("wheel_radius", wheel_radius_);  //轮半径
-        controller_nh.getParam("wheel_base", wheel_base_);   //轴距
+        controller_nh.getParam("wheel_base", wheel_base_);  //轴距
         controller_nh.getParam("wheel_track", wheel_track_);  //轮距
+        controller_nh.getParam("speed_mode", speed_mode_);  //速度模式
 
         //初始化 PID 控制器
         front_left_pid_.init(ros::NodeHandle(controller_nh, "front_left_pid"));
@@ -27,33 +28,43 @@ namespace hero_chassis_controller {
 
         // 订阅动作指令话题
         joint_state_sub = root_nh.subscribe("/joint_states", 10, &HeroChassisController::jointStateCallback, this);
+        //速度发布者
+//        cmd_vel_pub = root_nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
         //订阅当前速度
-        cmd_vel_sub = root_nh.subscribe("/cmd_vel", 1, &HeroChassisController::cmdVelCallback, this);
+        cmd_vel_sub= root_nh.subscribe("/cmd_vel", 10, &HeroChassisController::cmdVelCallback, this);
+        //创建发布者，将数据发以  nav_msgs/Odometry  发布到"/odom"上
+        odom_pub = root_nh.advertise<nav_msgs::Odometry>("/odom", 30);
 
-        odom_pub = root_nh.advertise<nav_msgs::Odometry>("/odom", 50);
-
-        last_cmd_time_ = ros::Time::now();  // 更新最新命令时间
+        last_cmd_time_ = ros::Time::now();  // 更新时间
         return true;
     }
 
+
+    //读取轮子的速度(不知道为什么，要是删掉这个函数，程序就不能运行了)
+    void HeroChassisController::jointStateCallback(const sensor_msgs::JointState::ConstPtr &msg) {
+        back_left_vel_ = msg->velocity[0];
+        front_left_vel_ = msg->velocity[1];
+        back_right_vel_ = msg->velocity[2];
+        front_right_vel_ = msg->velocity[3];
+    }
+
     void HeroChassisController::update(const ros::Time &time, const ros::Duration &period) {
+        ros::NodeHandle nh;
+        pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
-        // 获得整体的目标速度
-        double vx = goal_vx_;
-        double vy = goal_vy_;
-        double omega = goal_omega_;
-        //ROS_INFO("计算轮子目标速度: %f %f %f", vx, vy, omega);
-
-        //获得各个轮子的当前速度
         front_left_vel_ = front_right_joint_.getVelocity();
         front_right_vel_ = back_right_joint_.getVelocity();
         back_left_vel_ = front_left_joint_.getVelocity();
         back_right_vel_ = back_left_joint_.getVelocity();
         //四轮驱动的逆运动学公式
-        double front_left_target = (vx - vy - (wheel_track_ + wheel_base_) * omega) / wheel_radius_;
-        double front_right_target = (vx + vy + (wheel_track_ + wheel_base_) * omega) / wheel_radius_;
-        double back_left_target = (vx + vy - (wheel_track_ + wheel_base_) * omega) / wheel_radius_;
-        double back_right_target = (vx - vy + (wheel_track_ + wheel_base_) * omega) / wheel_radius_;
+        double front_left_target =
+                (target_vx_ - target_vy_ - (wheel_track_ + wheel_base_) * target_omega_)/wheel_radius_;
+        double front_right_target =
+                (target_vx_ + target_vy_ + (wheel_track_ + wheel_base_) * target_omega_)/wheel_radius_;
+        double back_left_target =
+                (target_vx_ + target_vy_ - (wheel_track_ + wheel_base_) * target_omega_)/wheel_radius_;
+        double back_right_target =
+                (target_vx_ - target_vy_ + (wheel_track_ + wheel_base_) * target_omega_)/wheel_radius_;
 
         //  PID 控制命令
         double front_left_cmd = front_left_pid_.computeCommand(front_left_target - front_left_vel_, period);
@@ -65,77 +76,117 @@ namespace hero_chassis_controller {
         front_right_joint_.setCommand(front_right_cmd);
         back_left_joint_.setCommand(back_left_cmd);
         back_right_joint_.setCommand(back_right_cmd);
+        //初始化msg
+        a_msg.linear.x = 0.1;
+        a_msg.linear.y = 0.1;
+        a_msg.linear.z = 0;
+        a_msg.angular.x = 0;
+        a_msg.angular.y = 0;
+        a_msg.angular.z = 0;
+        pub.publish(a_msg); // 发布消息
     }
 
-    //读取当前四个轮子速度
-    void HeroChassisController::jointStateCallback(const sensor_msgs::JointState::ConstPtr &msg) {
-        back_left_vel_ = msg->velocity[0];
-        front_left_vel_ = msg->velocity[1];
-        back_right_vel_ = msg->velocity[2];
-        front_right_vel_ = msg->velocity[3];
-    }
 
-
-    //读取/cmd_vel上的速度
-    void HeroChassisController::cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg) {
-        goal_vx_ = msg->linear.x;    // 整个底盘的x轴线速度（前进）
-        goal_vy_ = msg->linear.y;    // 整个底盘的y轴线速度（侧移）
-        goal_omega_ = msg->angular.z;    // 整个底盘的z轴角速度（旋转）
-
-        last_cmd_time_ = ros::Time::now(); // 更新时间
-    }
-
-//    void HeroChassisController::updateOdometry()
-//    {
-//        ros::Time current_time, last_time;
-//        current_time = ros::Time::now();
-//        last_time = ros::Time::now();
-//
-//        //compute odometry in a typical way given the velocities of the robot
-//        double dt = (current_time - last_time).toSec();
-//        double delta_x = (od_vx * cos(th) - od_vy * sin(th)) * dt;
-//        double delta_y = (od_vx * sin(th) + od_vy * cos(th)) * dt;
-//        double delta_th = od_vth * dt;
-//
-//        x += delta_x;
-//        y += delta_y;
-//        th += delta_th;
-//
-//
-//        //since all odometry is 6DOF we'll need a quaternion created from yaw
-//        geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
-//
-//        //first, we'll publish the transform over tf
-//        geometry_msgs::TransformStamped odom_trans;
-//        odom_trans.header.stamp = current_time;
-//        odom_trans.header.frame_id = "odom";
-//        odom_trans.child_frame_id = "base_link";
-//
-//        odom_trans.transform.translation.x = x;
-//        odom_trans.transform.translation.y = y;
-//        odom_trans.transform.translation.z = 0.0;
-//        odom_trans.transform.rotation = odom_quat;
-//
-//        // send the transform
-//        odom_broadcaster.sendTransform(odom_trans);
-//
-//        nav_msgs::Odometry odom_data{};
-//        odom_data.header.stamp = current_time;
-//        odom_data.header.frame_id = "odom";
-//
-//        odom_data.pose.pose.position.x = x;
-//        odom_data.pose.pose.position.y = y;
-//        odom_data.pose.pose.position.z = 0.0;
-//        odom_data.pose.pose.orientation = odom_quat;
-//
-//        odom_data.child_frame_id = "base_link";
-//        odom_data.twist.twist.linear.x = od_vx;
-//        odom_data.twist.twist.linear.y = od_vy;
-//        odom_data.twist.twist.angular.z = od_vth;
-//
-//        odom_pub.publish(odom_data);
-//        last_time = current_time;
+//    //读取/cmd_vel上的速度
+//    void HeroChassisController::cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg) {
+//        vx = msg->linear.x;    // 整个底盘的x轴线速度（前进）
+//        vy = msg->linear.y;    // 整个底盘的y轴线速度（侧移）
+//        omega = msg->angular.z;    // 整个底盘的z轴角速度（旋转）
+//        //以目前从/cmd_vel上读取的速度来计算当前每个轮子的速度
+//        double front_left_now = (vx - vy - (wheel_track_ + wheel_base_) * omega);
+//        double front_right_now = (vx + vy + (wheel_track_ + wheel_base_) * omega);
+//        double back_left_now = (vx + vy - (wheel_track_ + wheel_base_) * omega);
+//        double back_right_now = (vx - vy + (wheel_track_ + wheel_base_) * omega);
+//        ROS_INFO("Now speed:%f,%f,%f,%f", front_left_now, front_right_now, back_left_now, back_right_now);
+//        last_cmd_time_ = ros::Time::now(); // 更新时间
 //    }
+    // 获取当前cmd_vel上的速度指令
+    void HeroChassisController::cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg) {
+        // 获取当前机器人与世界坐标系的变换
+        if (speed_mode_ == "global") {
+            // 如果速度模式是全局坐标系(global)，则需要转换速度到底盘中，以控制底盘
+            tf::StampedTransform transform;
+            tf_listener.lookupTransform("base_link", "odom", ros::Time(0), transform);
+
+            // 进行坐标转换，将全局速度转换为底盘坐标系下的速度
+            tf::Vector3 global_velocity(vx, vy, omega);
+            tf::Vector3 local_velocity = transform.inverse() * global_velocity;
+            // 更新目标速度
+            vx = local_velocity.x();
+            vy = local_velocity.y();
+            omega = local_velocity.z();
+        } else {
+            // 如果速度模式是底盘坐标系(local)，则直接传入速度
+            vx = msg->linear.x;    // x轴线速度（前进）
+            vy = msg->linear.y;    // y轴线速度（侧移）
+            omega = msg->angular.z;    // 角速度（旋转）
+        }
+        //以目前从/cmd_vel上读取的速度来计算当前每个轮子的速度
+        double front_left_now = (vx - vy - (wheel_track_ + wheel_base_) * omega);
+        double front_right_now = (vx + vy + (wheel_track_ + wheel_base_) * omega);
+        double back_left_now = (vx + vy - (wheel_track_ + wheel_base_) * omega);
+        double back_right_now = (vx - vy + (wheel_track_ + wheel_base_) * omega);
+        ROS_INFO("Now speed:  %f  ,  %f  ,  %f  ,  %f  ", front_left_now, front_right_now, back_left_now, back_right_now);
+        last_cmd_time_ = ros::Time::now();  // 更新时间
+    }
+
+    void HeroChassisController::updateOdometry(const sensor_msgs::JointState::ConstPtr &msg) {
+        //根据车轮的速度计算里程计
+        double dt = (ros::Time::now() - last_cmd_time_).toSec();   // 根据时间差计算dt
+        last_cmd_time_ = ros::Time::now();              //记录last_cmd_time_用于计算dt
+
+        //获取各个车轮的速度
+        //  back_left_vel_ = msg->velocity[0];
+        //  front_left_vel_ = msg->velocity[1];
+        //  back_right_vel_ = msg->velocity[2];
+        //  front_right_vel_ = msg->velocity[3];
+        double v1 = msg->velocity[3];       //front_right
+        double v2 = msg->velocity[2];       //back_right
+        double v3 = msg->velocity[1];       //front_left
+        double v4 = msg->velocity[0];       //back_left
+
+        //使用微分思想进行计算与累加
+        dx = wheel_radius_ * 0.25 * (v1 + v2 + v3 + v4);
+        dy = wheel_radius_ * 0.25 * (v3 + v2 - v1 - v4);
+        dth = wheel_radius_ * (v1 + v2 - v3 - v4) / ((wheel_track_ / 2 + wheel_base_ / 2) * 4);
+
+        x += dx * cos(th) * dt - dy * sin(th) * dt;
+        y += dx * sin(th) * dt + dy * cos(th) * dt;
+        th += dth * dt;
+
+        //构造 nav_msgs::Odometry 消息来储存各种信息，并且将th转换为四元数
+        nav_msgs::Odometry odom_Data{};
+        geometry_msgs::Quaternion odom_quaternion = tf::createQuaternionMsgFromYaw(th);
+
+        odom_Data.header.stamp = ros::Time::now();
+        odom_Data.header.frame_id = "odom";
+
+        odom_Data.pose.pose.position.x = x;
+        odom_Data.pose.pose.position.y = y;
+        odom_Data.pose.pose.position.z = 0.0;
+        odom_Data.pose.pose.orientation = odom_quaternion;
+
+        odom_Data.child_frame_id = "base_link";
+        odom_Data.twist.twist.linear.x = dx;
+        odom_Data.twist.twist.linear.y = dy;
+        odom_Data.twist.twist.angular.z = dth;
+
+        odom_pub.publish(odom_Data);    //发布
+
+
+        //从"odom"到"base_link"的坐标变换关系
+        geometry_msgs::TransformStamped odom_Trans;
+        odom_Trans.header.stamp = ros::Time::now();
+        odom_Trans.header.frame_id = "odom";
+        odom_Trans.child_frame_id = "base_link";
+
+        odom_Trans.transform.translation.x = x;
+        odom_Trans.transform.translation.y = y;
+        odom_Trans.transform.translation.z = 0;
+        odom_Trans.transform.rotation = odom_quaternion;
+        //发送tf变换
+        odom_broadcaster.sendTransform(odom_Trans);
+    }
 
     PLUGINLIB_EXPORT_CLASS(hero_chassis_controller::HeroChassisController, controller_interface::ControllerBase)
 }  // namespace hero_chassis_controller
